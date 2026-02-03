4d15352a3a6d48635b2524fa4eea149fac533c65
Afe Abota <afeworkesayas163@gmail.com>
2026-02-02 18:37:20 -0800

    Add inventory system program to module2.cpp

---
+++ module2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Inventory System!" << endl;
+    cout << "\n\tData Types:\n";
+    cout << "Size of int: " << sizeof(int) << " bytes\n";
+    cout << "Size of float: " << sizeof(float) << " bytes\n";
+    cout << "Size of char: " << sizeof(char) << " bytes\n";
+    return 0;
+}
